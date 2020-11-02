7cf11ce9d20f92bc69b238a963bb80338c941515
Lucas <49034504+Altar2@users.noreply.github.com>
2020-11-02 14:46:56 -0500

    Add files via upload

--- GOAT.py
+++ GOAT.py
@@ -1,15 +1,19 @@
 import pandas as pd
+from tqdm import trange
+import time
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 
 def main():
-    print("GOAT starting...")
-    return GOAT()
-    print("GOAT COMPLETE")
+    print("Determining GOAT please wait...")
+    for i in trange(100):
+        time.sleep(.1)
+    print("The GOAT is " + GOAT())
 
 
 def GOAT():
-    
+    return "Bill Russell"
+
 
 
 
