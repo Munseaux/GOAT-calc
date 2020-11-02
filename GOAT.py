6a97cb5d09925734210011e9bddaa4dd635d4660
Lucas <49034504+Altar2@users.noreply.github.com>
2020-11-01 19:39:34 -0500

    Add files via upload

---
+++ GOAT.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playercareerstats
+
+def main():
+    print("GOAT starting...")
+    return GOAT()
+    print("GOAT COMPLETE")
+
+
+def GOAT():
+    
+
+
+
+
+
+
+#def total_offense_score(player_stats):
+
+
+#def total_defense_score(player_stats):
+
+
+#def total_playoff_offense(player_stats):
+
+
+#def total_playoff_defense(player_stats):
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
